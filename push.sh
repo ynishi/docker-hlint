472001e8dbcee4453278d93c2883ba020af158ef
Yutaka Nishimura <ytk.nishimura@gmail.com>
2019-09-28 22:27:01 +0900

    support submodule tag version

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-VERSION=$(git branch | awk '/\*/ { print $2; }' | grep -v master)
+VERSION=$(cd hlint && git branch | awk '/\*/ { print $2; }' | grep -v master)
 
 docker build --cache-from builder -t ynishi/docker-hlint:latest .
 docker build --cache-from builder -t ynishi/docker-hlint:${VERSION} .
